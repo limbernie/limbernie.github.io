f3b5dd362d2d92cc6902886fe6e1a8496131c266
root <lim.yiok.kia@gmail.com>
2018-02-05 07:29:38 +0000

    back to top

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
 $(document).ready(function() {
+	var offset = 50;
+	var duration = 500;
+	$('.post-listing').scroll(function() {
+		if ($(this).scrollTop() > offset) {
+			$('.back-to-top').fadeIn(duration);
+		} else {
+			$('.back-to-top').fadeOut(duration);
+		}
+	});
+	$('.back-to-top').click(function() {
+		$('.post-listing').animate({
+			scrollTop: 0}, 600);
+	});
 });
