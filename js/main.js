4f6a73242eee9a77ff82489605186930cd97385f
root <lim.yiok.kia@gmail.com>
2018-02-06 07:26:18 +0000

    back to top

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
 $(document).ready(function() {
 	var offset = 250;
 	var duration = 500;
-	$('.post-listing').scroll(function() {
-		if ($(this).scrollTop() > offset) {
-			$('.back-to-top').fadeIn(duration);
-		} else {
-			$('.back-to-top').fadeOut(duration);
-		}
-	});
-	$('.back-to-top').click(function() {
-		$('.post-listing').animate({
-			scrollTop: 0}, 600);
-	});
+	if ($(window).width() > 977) {
+		$('.post-listing').scroll(function() {
+			if ($(this).scrollTop() > offset) {
+				$('.back-to-top').fadeIn(duration);
+			} else {
+				$('.back-to-top').fadeOut(duration);
+			}
+		});
+		$('.back-to-top').click(function() {
+			$('.post-listing').animate({
+				scrollTop: 0}, 600);
+		});
+	} else {
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > offset) {
+				$('.back-to-top').fadeIn(duration);
+			} else {
+				$('.back-to-top').fadeOut(duration);
+			}
+		});
+		$('.back-to-top').click(function() {
+			$('html, body').animate({
+				scrollTop: 0}, 600);
+		});
+	}
 });
