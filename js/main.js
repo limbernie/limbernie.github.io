83f4c829095eb12da89230c0677dd6f2b2b05efd
Bernie Lim <lim.yiok.kia@gmail.com>
2018-03-21 15:37:03 +0000

    back to top

--- js/main.js
+++ js/main.js
@@ -3,12 +3,7 @@ $(document).ready(function() {
 	var max_width = 977;  // trial-n-error
 	var width = $(window).width();
 
-	backToTop(width);
-
-	$(window).resize(function() {
-		location.reload(); // is this the best?
-		backToTop(width());
-	});
+	if (width < max_width) backToTop(width);
 
 	function backToTop(width) {
 		var offset = 400;
@@ -17,30 +12,16 @@ $(document).ready(function() {
 		if ( $('.feature-image').length ) offset += $('.feature-image').height();
 		if ( $('.notice').length ) offset += $('.notice').height();
 
-		if (width > max_width) {
-			$('.post-listing').scroll(function() {
-				if ($(this).scrollTop() > offset - 20) {
-					$('.back-to-top').fadeIn(duration);
-				} else {
-					$('.back-to-top').fadeOut(duration);
-				}
-			});
-			$('.back-to-top').click(function() {
-				$('.post-listing').animate({
-					scrollTop: 0}, duration);
-			});
-		} else {
-			$(window).scroll(function() {
-				if ($(this).scrollTop() > offset + 340) {
-					$('.back-to-top').fadeIn(duration);
-				} else {
-					$('.back-to-top').fadeOut(duration);
-				}
-			});
-			$('.back-to-top').click(function() {
-				$('html, body').animate({
-					scrollTop: 400}, duration);
-			});
-		}
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > offset + 340) {
+				$('.back-to-top').fadeIn(duration);
+			} else {
+				$('.back-to-top').fadeOut(duration);
+			}
+		});
+		$('.back-to-top').click(function() {
+			$('html, body').animate({
+				scrollTop: 400}, duration);
+		});
 	}
 });
