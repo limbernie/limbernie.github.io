454d4c6ffb1399abb1a5822d3844cced87147095
Bernie Lim <lim.yiok.kia@gmail.com>
2018-03-26 15:56:36 +0000

    back to top

--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
-var max_width = 977;  // trial-n-error
-
 $(document).ready(function() {
-
-	width = $(window).width();
+	var width = $(window).width();
 	backToTop(width);
 });
 
-$(window).resize(function() {
-	backToTop($(window).width());
-});
+$(window).resize(debounce(function() {
+	var width = $(window).width();
+	backToTop(width);
+}, 500));
 
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
 
 function backToTop(width) {
+	var max_width = 977;  // trial-n-error
 	var offset = 400;
 	var duration = 500;
 	var btt = $('.back-to-top');
@@ -22,38 +35,26 @@ function backToTop(width) {
 	if (width > max_width) {
 		$('.post-listing').scroll(function() {
 			if ($(this).scrollTop() > Math.ceil(offset - 56)) {
-				btt.fadeIn(duration, function() {
-					btt.stop(true, false);
-				});
+				btt.fadeIn(duration);
 			} else {
-				btt.fadeOut(duration, function() {
-					btt.stop(true, false);
-				});
+				btt.fadeOut(duration);
 			}
 		});
 		btt.click(function() {
 			$('.post-listing').animate({
-				scrollTop: 0}, duration, function() {
-					$('.post-listing').stop(true, false);
-			});
+				scrollTop: 0}, duration);
 		});
 	} else {
 		$(window).scroll(function() {
 			if ($(this).scrollTop() > offset + 340) {
-				btt.fadeIn(duration, function() {
-					btt.stop(true, false);
-				});
+				btt.fadeIn(duration);
 			} else {
-				btt.fadeOut(duration, function() {
-					btt.stop(true, false);
-				});
+				btt.fadeOut(duration);
 			}
 		});
 		btt.click(function() {
 			$('html, body').animate({
-				scrollTop: 0}, duration, function() {
-					$('html, body').stop(true, false);
-			});
+				scrollTop: 0}, duration);
 		});
 	}
 }
