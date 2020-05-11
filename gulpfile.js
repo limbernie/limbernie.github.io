0cb7749e210b4d3a4873495dc3fccea90daab76e
Bernie Lim <limbernie@pm.me>
2020-05-11 01:16:54 +0000

    getting ready for cloudinary

--- gulpfile.js
+++ gulpfile.js
@@ -38,21 +38,21 @@ function encrypt(password) {
 			var delimiter = '---',
 			chunks = String(file.contents).split(delimiter),
 			frontMatter = chunks[1];
-			
+
 			if (!hasProtect(frontMatter)) {
 				this.emit('error', new PluginError({
 					plugin: 'Protect',
 					message: 'Front Matter "protect: true" not found.'
 				}));
 			}
-			
+
 			var dom = new JSDOM(String(fs.readFileSync(index))),
 				originalBody = dom.window.document.querySelector('.post-content').outerHTML,
 				encryptedBody = cryptojs.AES.encrypt(originalBody, password),
 				hmac = cryptojs.HmacSHA256(encryptedBody.toString(), cryptojs.SHA256(password).toString()).toString(),
 				encryptedFrontMatter = 'encrypted: ' + hmac + encryptedBody,
 				result = [ delimiter, frontMatter, encryptedFrontMatter, '\n', delimiter ];
-			
+
 			file.contents = new Buffer(result.join(''));
 			this.push(file);
 			return callback();
@@ -85,7 +85,7 @@ gulp.task('encrypt', () => {
 });
 
 gulp.task('upload', () => {
-	return gulp.src(options.path.replace(/\/$/, '') + '/*')
+	return gulp.src(options.file ? options.file : options.path.replace(/\/$/, '') + '/*')
 		.pipe(cloudinary({
 			params: {
 				folder: options.folder
